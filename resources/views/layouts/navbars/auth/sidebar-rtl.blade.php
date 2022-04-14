e1e364b6e04c1de65e0df8e62b820c720503fd8c
ovidiustanc123 <ovidiu.stanc@updivision.com>
2022-04-14 13:05:44 +0300

    navs-title-update

--- resources/views/layouts/navbars/auth/sidebar-rtl.blade.php
+++ resources/views/layouts/navbars/auth/sidebar-rtl.blade.php
@@ -1,9 +1,9 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-end me-3 rotate-caret " id="sidenav-main">
   <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute start-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-    <a class="navbar-brand m-0" href="https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html" target="_blank">
-      <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-      <span class="me-1 font-weight-bold">Soft UI Dashboard</span>
+    <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
+        <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="...">
+        <span class="ms-1 font-weight-bold">Soft UI Dashboard Laravel</span>
     </a>
   </div>
   <hr class="horizontal dark mt-0">
--- resources/views/layouts/navbars/guest/nav.blade.php
+++ resources/views/layouts/navbars/guest/nav.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 my-3 {{ (Request::is('static-sign-up') ? 'w-100 shadow-none  navbar-transparent mt-4' : 'blur blur-rounded shadow py-2 start-0 end-0 mx4') }}">
   <div class="container-fluid {{ (Request::is('static-sign-up') ? 'container' : 'container-fluid') }}">
     <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 {{ (Request::is('static-sign-up') ? 'text-white' : '') }}" href="{{ url('dashboard') }}">
-      Soft UI Dashboard
+      Soft UI Dashboard Laravel
     </a>
     <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon mt-2">
