22873e8b6e54f4926051079ef1d68cc9ab08e417
2Reis <2reis.developer@gmail.com>
2023-04-11 19:19:17 -0300

    Requisicao de Arte, model, view, controller e rota

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Form;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+{
+    $data = $request->all();
+    
+        $data['status'] = 'request';
+        $data['category'] = 'requisition';
+        $user = Auth::user();
+        $data['user_email'] = $user->email;
+        Form::create($data);
+        return back()->with('success','Requisição cadastrada com sucesso.');
+}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/requisicao-de-arte.blade.php
+++ resources/views/requisicao-de-arte.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.user_type.auth')
-<script src="../assets/js/plugins/jquery.js"></script>
 @section('content')
     <div class="row" id="contentArt">
         <div class="col-lg-12 col-md-12 mb-12">
@@ -7,20 +6,70 @@
                 <div class="card-body p-3">
                     <div class="d-flex mb-2">
                         <div class="avatar avatar-xl bg-gradient-warning border-radius-md p-2">
-                            <i class="fa fa-list" text-lg opacity-10" aria-hidden="true"></i>
+                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
                         </div>
                         <div class="ms-3 my-auto">
-                            <h6>Pedidos da Loja</h6>
-                            <p class="text-sm mt-2">Essa página é apenas para consulta e facilidade de copiar o código ou
-                                número do pedido e verificar o status do mesmo.</p>
+                            <h6>Requisição de Arte.</h6>
+                            <p>Preencha os campos para requisição de arte. As artes terão no mínimo 2 dias de produção.</p>
                         </div>
                     </div>
-                    {{ toMoney('56.00') }}
+                    <form method="GET" action="{{ route('requisicao-de-arte/storeform') }}">
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Ideia/Arte.</label>
+                            <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="url" class="form-label">Referência (opcional) - Link de imagem.</label>
+                            <input type="varchar" class="form-control" id="url" name="url" placeholder="Insira o link de imagem aqui">
+                        </div>
+                        <div class="mb-3">
+                            <p class="form-label">Precisa de data para a arte?</p>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" id="date_requisition_sim" name="precisa_data" value="sim">
+                                <label class="form-check-label" for="date_requisition_sim">
+                                    Sim
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" id="date_requisition_nao" name="precisa_data" value="nao" checked>
+                                <label class="form-check-label" for="date_requisition_nao">
+                                    Não
+                                </label>
+                            </div>
+                        </div>
+                        <div class="mb-3" id="div-data" style="display: none;">
+                            <label for="data" class="form-label">Data de entrega (Mínimo 2 dias úteis de produção)</label>
+                            <input type="date" class="form-control" id="data" name="date_requisition" disabled>
+                        </div>
+                        <script>
+                            const precisaDataSim = document.getElementById('date_requisition_sim');
+                            const precisaDataNao = document.getElementById('date_requisition_nao');
+                            const divData = document.getElementById('div-data');
+                            const inputData = document.getElementById('data');
+                            
+                            precisaDataSim.addEventListener('click', function() {
+                                divData.style.display = 'block';
+                                inputData.disabled = false;
+                                inputData.required = 'required';
+                            });
+                            
+                            precisaDataNao.addEventListener('click', function() {
+                                divData.style.display = 'none';
+                                inputData.disabled = true;
+                                inputData.required = '';
+                            });
+                        </script>
+                        
+                        <div class="mb-3">
+                            <label for="obs " class="form-label">Precisa de dados de loja? Insira os dados necessários.</label>
+                            <textarea class="form-control" id="obs" name="obs" rows="3" required></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Solicitar Arte</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 @endsection
-
 @push('dashboard')
 @endpush
