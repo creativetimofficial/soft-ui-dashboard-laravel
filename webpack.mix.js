c7fc51c3cddb9bb2f2c82c0f44e8c2270c9eaeaf
adela.boldeanu <adela.boldeanu@updivision.com>
2023-05-17 11:55:46 +0300

    minify js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ]).minify('public/assets/js/soft-ui-dashboard.js');
 mix.sass('public/assets/scss/soft-ui-dashboard.scss', 'public/assets/css');
